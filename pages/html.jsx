9d72481122040f403ea57f7e65c8a57add5ce385
MagicSalt <darrenlin815@hotmail.com>
2023-01-23 18:28:34 -0800

    Adds a pages folder where homepage is located and updates the package.json file to run a development server

--- pages/html.jsx
+++ pages/html.jsx
@@ -4,7 +4,7 @@ const Header = ({ title }) => {
   return <h1>{title ? title: 'Default title'}</h1>;
 }
 
-const Homepage= () => {
+export default Homepage= () => {
   const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
 
   const [likes, setLikes] = useState(0);
